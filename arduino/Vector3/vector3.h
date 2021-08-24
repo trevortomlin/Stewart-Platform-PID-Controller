8afd699661c89d97da4f6e9b9da199d1d78bb48c
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2021-08-24 10:02:06 -0700

    Added libraries

---
+++ arduino/Vector3/vector3.h
@@ -0,0 +1,32 @@
+#ifndef vector3
+#define vector3
+
+#if (ARDUINO >= 100)
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+class Vector3 {
+  
+public:
+
+  double x, y, z;
+
+  Vector3();
+  Vector3(double _x, double _y, double _z);
+
+  void operator=(const Vector3 &v);
+
+  Vector3 operator+(Vector3 &v);
+  Vector3 operator-(Vector3 &v);
+
+  Vector3 operator*(double &s);
+  Vector3 operator*(Vector3 &v);
+  Vector3 operator*(double mat3[3][3]);
+
+  double mag();
+
+};
+
+#endif
