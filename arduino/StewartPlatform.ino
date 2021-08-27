0a8a129dd886908e2c10b589141f719b692fd9eb
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2021-08-26 18:53:30 -0700

    Added calculation of platform on device and writing to servos.

--- arduino/StewartPlatform.ino
+++ arduino/StewartPlatform.ino
@@ -1,12 +1,54 @@
 #include <vector3.h>
 #include <StewartPlatform.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVO_FREQ 50
+
+StewartPlatform s;
 
 void setup() {
-
-  StewartPlatform s;
+    
+    Serial.begin(9600);
+    pwm.begin();
+    pwm.setPWMFreq(SERVO_FREQ);
+    delay(10);
 
 }
 
 void loop() {
+
+    Vector3 r(30, 0, 222.11-40.0);
+    s.moveTo(r, 0, 0, 0);
+    writeServos(s.W);
+    
+    delay(5000);
+
+    Vector3 t(0, 0, 222.11-40.0);
+    s.moveTo(t, 0, 0, 0);
+    writeServos(s.W);
+
+    delay(5000);
+
+    Vector3 e(-25, 0, 222.11-40.0);
+    s.moveTo(e, 0, 0, 0);
+    writeServos(s.W);
+
+    delay(5000);
+
+
+}
+
+void writeServos(double pos[6]){
+
+    for (int i=0; i < 6; i++){
+	
+	Serial.println(pos[i]);
+	pwm.writeMicroseconds(i, pos[i]);
+
+    }
+
 
 }
