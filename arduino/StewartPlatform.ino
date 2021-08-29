130cbfb56332a77eb5f32ad0ff959456330fac10
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2021-08-28 18:32:33 -0700

    Added function to get touchscreen coords.

--- arduino/StewartPlatform.ino
+++ arduino/StewartPlatform.ino
@@ -7,9 +7,20 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVO_FREQ 50
 
+#define LL 12
+#define UL 13
+#define LR 11
+#define UR 10
+#define S A0
+
 StewartPlatform s;
 
 void setup() {
+    
+    pinMode(LL, OUTPUT); 
+    pinMode(UL, OUTPUT); 
+    pinMode(LR, OUTPUT); 
+    pinMode(UR, OUTPUT); 
     
     Serial.begin(9600);
     pwm.begin();
@@ -19,6 +30,10 @@ void setup() {
 }
 
 void loop() {
+
+    int x, y;
+
+    getTouchScreenPos(x, y);
 
     Vector3 r(30, 0, 222.11-40.0);
     s.moveTo(r, 0, 0, 0);
@@ -45,10 +60,33 @@ void writeServos(double pos[6]){
 
     for (int i=0; i < 6; i++){
 	
-	Serial.println(pos[i]);
+	//Serial.println(pos[i]);
 	pwm.writeMicroseconds(i, pos[i]);
 
     }
 
 
+}
+
+void getTouchScreenPos(int &x, int &y){
+
+    digitalWrite(UR, HIGH);
+    digitalWrite(LR, HIGH);
+    digitalWrite(UL, LOW);
+    digitalWrite(LL, LOW);
+    
+    delay(10);
+    x = analogRead(S);
+
+    digitalWrite(UR, HIGH);
+    digitalWrite(LR, LOW);
+    digitalWrite(UL, HIGH);
+    digitalWrite(LL, LOW);
+    
+    delay(10);
+    y = analogRead(S);
+    
+    Serial.print(x);  
+    Serial.print(",");
+    Serial.println(y);
 }
